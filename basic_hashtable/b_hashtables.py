953e491b1a8c1d368b30eb4853c61a1737bd2850
Stefan <steaclem@gmail.com>
2019-07-24 19:23:22 -0700

    Finally figured out a pattern almost.

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -1,8 +1,8 @@
-
-
 # '''
 # Basic hash table key/value pair
 # '''
+
+
 class Pair:
     def __init__(self, key, value):
         self.key = key
@@ -16,7 +16,7 @@ class Pair:
 class BasicHashTable:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.count = 0
+        self.storage = [None] * capacity
         pass
 
 
@@ -24,20 +24,37 @@ class BasicHashTable:
 # Fill this in.
 # Research and implement the djb2 hash function
 # '''
-def hash(string, max):
-    hashed = 5381
+def hash(string):
+    hash = 5381
+
     for i in string:
-        hashed = ((hashed << 0) + hashed) + ord(string) 
-    return hash & 0xFFFFFFFF
-
-
+        hash = (((hash << 5) + hash) + ord(i)) & 0xFFFFFFFF
+    return hash
 # '''
 # Fill this in.
 
 # If you are overwriting a value with a different key, print a warning.
 # '''
+
+
 def hash_table_insert(hash_table, key, value):
-    pass
+    key_hash = hash(key)
+    key_value = Pair(key, value)
+
+    # to get the index you need take the has modulus by the size(capcity)
+    index = key_hash % hash_table.capacity
+    # we want to check if the storage is not None
+    if hash_table.storage[index] is not None:
+        # If the key matches the key in the storage
+        if key == hash_table.storage[index].key:
+            # Then we want to know that it's a collision
+            print(f'Collision found with {key}')
+        else:
+            # Otherwise we can take the value and set it to our storage value
+            hash_table.storage[index].value = value
+    else:
+        # if storage is none then set the store to the value pair
+        hash_table.storage[index] = key_value
 
 
 # '''
@@ -46,7 +63,17 @@ def hash_table_insert(hash_table, key, value):
 # If you try to remove a value that isn't there, print a warning.
 # '''
 def hash_table_remove(hash_table, key):
-    pass
+    # same here we want to get the hashed key
+    key_hash = hash(key)
+    # Take the hash key and modulus with size(capacity) to get the index
+    index = key_hash % hash_table.capacity
+    # Take the index of the storage and verify if its set to None
+    if hash_table.storage[index] is not None:
+        # if the hash storage is not none. Then set it to None
+        hash_table.storage[index] = None
+    else:
+        # If it is none then we can send a message to Say There is not a key in the table
+        print(f'{key} is not in the hash table. So it can not be removed')
 
 
 # '''
@@ -55,7 +82,18 @@ def hash_table_remove(hash_table, key):
 # Should return None if the key is not found.
 # '''
 def hash_table_retrieve(hash_table, key):
-    pass
+     # same here we want to get the hashed key
+    key_hash = hash(key)
+    # Take the hash key and modulus with size(capacity) to get the index
+    index = key_hash % hash_table.capacity
+    # Take the index of the storage and verify if its set to None
+    if hash_table.storage[index] is not None:
+        # if it not none then check if the key is = to the storage key
+        if hash_table.storage[index].key == key:
+            # if it is = to storage key return the value in storage
+            return hash_table.storage[index].value
+
+    return None
 
 
 def Testing():
